6661283d12f9a04f814195b0a94a2654fffa5f96
luciano-antonio <luciano.a.antonio@gmail.com>
2026-03-28 16:39:28 -0300

    fear: banco de dados funcionando

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import express, { Request, Response } from 'express'
 const app = express()
 app.use(express.json())
 import { Pool } from 'pg'
-import crypto   from 'crypto'
+import crypto from 'crypto'
 
 const pool = new Pool({
     user: process.env.DB_USER,
@@ -20,6 +20,7 @@ app.get('/login', async (req: Request, res: Response) => {
 
 let sessionKey = ''
 let usuarioLogado = ''
+let usuarioID = ''  // variavel global apenas pra coletar id da rota callback
 
 app.get('/callback', async (req: Request, res: Response) => {
     const token = req.query.token as string
@@ -28,10 +29,19 @@ app.get('/callback', async (req: Request, res: Response) => {
     .update(`api_key${process.env.LASTFM_API_KEY}methodauth.getSessiontoken${token}${process.env.LASTFM_SECRET}`)
     .digest('hex')
 
-const valido =  await fetch(`https://ws.audioscrobbler.com/2.0/?method=auth.getSession&api_key=${process.env.LASTFM_API_KEY}&token=${token}&api_sig=${assinatura}&format=json`)
+    const valido =  await fetch(`https://ws.audioscrobbler.com/2.0/?method=auth.getSession&api_key=${process.env.LASTFM_API_KEY}&token=${token}&api_sig=${assinatura}&format=json`)
     const data = await valido.json()
     sessionKey = data.session.key
     usuarioLogado = data.session.name
+
+const inserirBanco = await pool.query(`INSERT INTO usuarios (username, session_key)
+    VALUES ($1, $2)
+    ON CONFLICT (username)
+    DO UPDATE SET session_key = $2
+    RETURNING id`,
+    [usuarioLogado, sessionKey])
+
+    usuarioID = inserirBanco.rows[0].id
     res.json(data)
 })
 
@@ -47,6 +57,10 @@ app.get('/top-tracks', async (req: Request, res: Response) => {
 
     }))
     
+    filtrar.forEach(async (track: any) => {
+        await pool.query(`INSERT INTO historico_tracks (usuario_id, nome_musica, artista, plays) VALUES ($1, $2, $3, $4)`, [usuarioID, track.nome, track.artista, track.plays])
+    })
+
     res.json(filtrar)
 })
 
@@ -59,6 +73,11 @@ app.get('/top-artists', async (req: Request, res: Response) => {
         ranking: (artist as any)['@attr'].rank,
         playcount: artist.playcount
     }))
+
+    filtro.forEach(async (artist: any) => {
+        await pool.query(`INSERT INTO historico_artists (usuario_id, artista, plays) VALUES ($1, $2, $3)`, [usuarioID, artist.artista, artist.playcount])
+    })
+
     res.json(filtro)
 })
 
